c07cd71e5e286e793d82cf0234acab79644e61ad
Rami Kassouf <Rami_kassouf@hotmail.com>
2023-06-13 16:35:22 +0300

    modified

--- dags/producer.py
+++ dags/producer.py
@@ -4,6 +4,8 @@ from airflow.decorators import task
 from datetime import datetime
 
 my_file= Dataset("/tmp/my_file.txt")
+my_file_2= Dataset("/tmp/my_file_2.txt")
+
 
 # Create DAG
 
@@ -17,6 +19,11 @@ with DAG(
     def update_dataset():
         with open(my_file.uri, "a+") as f:
             f.write("producer update")
+
+    @task(outlets=[my_file_2])
+    def update_dataset2():
+        with open(my_file_2.uri, "a+") as f:
+            f.write("producer update")
             
 
-    update_dataset()
+    update_dataset() >> update_dataset2()
